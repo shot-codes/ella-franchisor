37e0c7fc7f7e78361e87813792c4e107fa0ed759
Nicolai <nicolai.verbaarschot@gmail.com>
2022-12-26 16:19:45 +0100

    Removed migtrations.

---
+++ prisma/migrations/20221226151802_init/migration.sql
@@ -0,0 +1,35 @@
+-- CreateTable
+CREATE TABLE "user" (
+    "id" TEXT NOT NULL,
+    "provider_id" TEXT NOT NULL,
+    "hashed_password" TEXT,
+    "username" TEXT NOT NULL,
+    "email" TEXT,
+
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "session" (
+    "id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "expires" BIGINT NOT NULL,
+    "idle_expires" BIGINT NOT NULL,
+
+    CONSTRAINT "session_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_id_key" ON "user"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_provider_id_key" ON "user"("provider_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "session_id_key" ON "session"("id");
+
+-- CreateIndex
+CREATE INDEX "session_user_id_idx" ON "session"("user_id");
+
+-- AddForeignKey
+ALTER TABLE "session" ADD CONSTRAINT "session_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
