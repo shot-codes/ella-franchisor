76665f4b1ddfd49fb5c6ffef3869879fc6159408
Nicolai <nicolai.verbaarschot@gmail.com>
2022-12-27 02:45:12 +0100

    refactored route protection to hooks.

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,20 +1,19 @@
-// import { auth } from "$lib/server/lucia";
-// import { handleHooks } from "@lucia-auth/sveltekit";
-// import { redirect } from "@sveltejs/kit";
-// import { sequence } from "@sveltejs/kit/hooks";
-
-// export const handle = sequence(handleHooks(auth), async ({ event, resolve }) => {
-
-//     if (event.url.pathname != "/login") {
-//         const session = await event.locals.validate();
-//         if (!session) throw redirect(302, "/login");
-//     }
-
-//     const response = await resolve(event);
-//     return response;
-// });
-
 import { auth } from "$lib/server/lucia";
 import { handleHooks } from "@lucia-auth/sveltekit";
+import { redirect } from "@sveltejs/kit";
+import { sequence } from "@sveltejs/kit/hooks";
 
-export const handle = handleHooks(auth);
+export const handle = sequence(
+  handleHooks(auth),
+  async ({ event, resolve }) => {
+    if (
+      event.url.pathname !== "/login" &&
+      !event.url.pathname.includes("/auth/oauth")
+    ) {
+      const session = await event.locals.validate();
+      if (!session) throw redirect(302, "/login");
+    }
+
+    return await resolve(event);
+  }
+);
